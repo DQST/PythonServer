97edd3cb2338e5b079430b45b3f6bb87426ee7af
Dmitriy Yushko <rafiek57@gmail.com>
2016-03-14 00:38:09 +0300

    Update "UDP server". Be created class "Server", add methods in him.

--- UDP Server/server.py
+++ UDP Server/server.py
@@ -1,29 +1,51 @@
 import socket
 import time
 
-print("Server running...")
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("", 14800))
 
-last_time = 0
+class Server:
+	roomList = {}
+	roomID = 0
+	sock = None
+	last_time = 0
+	
+	def __init__(self, port):
+		print("Server running...")
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.sock.bind(("", port))
 
-while True:
-	data = sock.recvfrom(1024)
-	current_time = time.time()
-	ping = current_time - last_time
-	last_time = current_time
+	def Recieve(self):
+		while True:
+			data = self.sock.recvfrom(1024)
+			current_time = time.time()
+			ping = current_time - self.last_time
+			self.last_time = current_time
+			
+			# print("Ping: " + str(ping))
+			
+			msg = data[0].decode("utf-8")
+			addr = data[1]
+			
+			if not msg:
+				pass
+			else:
+				arr = msg.split(":")
+				if arr[0] == "nop":
+					continue
+				elif arr[0] == "createroom":
+					self.roomList[arr[1]]={'id':self.roomID,'host':addr[0],'port':int(addr[1])}
+					self.roomID += 1;
+					self.Send(bytes("Add new room:" + arr[1], 'utf-8'), addr)
+					continue
+				
+				# print("[" + addr[0] + ":" + str(addr[1]) + "] > " + msg)
+				self.Send(b"Hello from UDP Python Server!", addr)
 	
-	print("Ping: " + str(ping))
+	def Send(self, msg, remoteAddr):
+		self.sock.sendto(msg, remoteAddr)
 	
-	msg = data[0].decode("utf-8")
-	addr = data[1]
-	
-	if not msg:
-		pass
-	else:
-		if msg == "nop":
-			continue
-		print("[" + addr[0] + ":" + str(addr[1]) + "] > " + msg)
-		sock.sendto(b"Hello from UDP Python Server!", addr)
+	def Close(self):
+		self.sock.close()
 
-sock.close()
+server = Server(14801)
+server.Recieve()
+server.Close()
