4d26241c24cbc8d096d5d336a0e530f68351688f
Dmitriy Yushko <rafiek57@gmail.com>
2016-03-17 23:06:39 +0300

    Update "Server" class in "UDP Server/serv2.py", also add new class "UserData".

--- UDP Server/serv2.py
+++ UDP Server/serv2.py
@@ -4,6 +4,16 @@ from time import *
 from socket import *
 from sys import argv
 
+class UserData:
+	"""docstring for User"""
+	def __init__(self, ip, ping, time):
+		self.IPAddr = ip
+		self.Ping = ping
+		self.Time = time
+
+	def ToString(self):
+		return '|\t{0}\t|\t{1}ms\t|'.format(self.IPAddr, round(self.Ping, 2))
+		
 
 class Server:
 	'''Server constructor'''
@@ -16,22 +26,17 @@ class Server:
 		self.UserList = {}
 		self.ROOM_HASH = {}
 
-	def ParseData(self, inputData, ping):
+	def ParseData(self, inputData, ping, time):
 		data, addr = inputData
 		message = data.decode('utf-8')
 
 		ip_addr = str(addr[0]) + ':' + str(addr[1])
 
-		# command = message.split(':')
-		# if command[0] == '0042nop':
-		# 	self.UserList = [command[1], ip, ping]
-		# elif command[0] == '0042mkroom':
-		# 	self.ROOM_HASH[command[1]] = ip
-		# elif command[0] == '0042delroom':
-		# 	del self.ROOM_HASH[command[1]]
-		# elif command[0] == '0042conto':
-		# 	room_ip = self.ROOM_HASH[command[1]]
-		# 	self.SendTo('0042roomip:' + command[1] + ':' + room_ip, addr)
+		command = message.split(':')
+		if command[0] == '0042nop':
+		 	self.UserList[command[1]] = UserData(ip_addr, ping, time)
+		else:
+			self.SendTo("Hello from Server!", addr)
 
 	'''Send message here'''
 	def SendTo(self, msg, endPoint):
@@ -46,20 +51,36 @@ class Server:
 			self.lastTime = curTime
 
 			'''Parse input data'''
-			self.ParseData(inputData)
+			self.ParseData(inputData, PING, curTime)
 		else:
-			print('Recieve be stoped.')
+			print('Recieve stoped.')
+
+	def cls(self):
+		system('cls' if name == 'nt' else 'clear')
+
+	def ShowUserList(self):
+		if len(self.UserList) <= 0:
+			print('*********** No connecting ***********')
+
+		for i in self.UserList:
+			print('|\t%s\t%s' % (i, self.UserList[i].ToString()))
 
 	'''At this must be show data about clients'''
 	def Update(self):
-		for i in self.UserList:
-			ping = (time() - self.UserList[i][2]) * 100
-			if ping >= 100:
-				del self.UserList[i]
+		while  self.WORK:
+			print('Connecting to server:')
+			self.ShowUserList()
+			for i in self.UserList:
+				delta = (time() - self.UserList[i].Time) * 100
+				if delta >= 100:
+			 		del self.UserList[i]
+			 		if len(self.UserList) > 0:
+			 			continue
+			 		else:
+			 			break
 
-		for i in self.UserList:
-			print('|\t' + i + '\t|\t' + self.UserList[i][1] +
-				'\t|\t' + self.UserList[i][2]+ '\t|')
+			sleep(1)
+			self.cls()
 
 	'''Stop server here'''
 	def Stop(self):
