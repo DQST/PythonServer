a10e69567a9b690a3372444df0aa0e9204e00f7c
Dmitriy Yushko <rafiek57@gmail.com>
2016-05-26 21:35:15 +0300

    Add class "FileServer" in "Serv/run.py"

--- Serv/run.py
+++ Serv/run.py
@@ -57,6 +57,53 @@ class Users:
         return self.__users__
 
 
+class FileServer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(('0.0.0.0', 14801))
+        self.sock.listen(10)
+        self.__WORK__ = True
+        self.__I__ = 0
+
+    def run(self):
+        while self.__WORK__:
+            try:
+                conn, addr = self.sock.accept()
+                conn.settimeout(60)
+                while True:
+                    data = conn.recv(1028)
+                    head = data[:4]
+                    if head == b'\x00\x00\x00\x01':
+                        path = os.getcwd() + '\\downloads\\.part_' + str(self.__I__)
+                        f = open(path, 'ab')
+                        f.write(data[4:])
+                        f.close()
+
+                    if head == b'0002':
+                        h, file_name = data.decode('utf-8').split(':')
+                        path = os.getcwd() + '\\downloads\\'
+                        if os.path.exists(path + '.part_' + str(self.__I__)) is True:
+                            os.rename(path + '.part_' + str(self.__I__), path + file_name)
+
+                    if not data:
+                        break
+                self.__I__ += 1
+                conn.close()
+            except Exception as error:
+                logging.warning('----------------------------------------')
+                logging.warning('Type: "%s"' % type(error))
+                logging.warning('Exception: "%s"' % str(error))
+                logging.warning('Exception args: "%s"' % str(error.args))
+                logging.warning('----------------------------------------')
+
+    def stop(self):
+        self.__WORK__ = False
+        self.sock.close()
+        super()._stop()
+
+
 class Server(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
@@ -205,6 +252,8 @@ class RoomManager:
 
 
 if __name__ == '__main__':
+    files = FileServer()
+    files.start()
     server = Server()
     server.start()
     while True:
@@ -212,6 +261,8 @@ if __name__ == '__main__':
         if command == 'exit':
             server.stop()
             server.join()
+            files.stop()
+            files.join()
             break
         else:
             print('Unknown command "%s"' % command)
