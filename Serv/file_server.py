c85ca713839a3a20c5b1ef4a2b5367440acb6b55
Dmitriy Yushko <rafiek57@gmail.com>
2016-06-13 20:05:14 +0300

    Big update for "Serv/file_server.py"

--- Serv/file_server.py
+++ Serv/file_server.py
@@ -1,11 +1,13 @@
 import socket
 
-i = 0
 path = 'downloads/'
+state = ''
+select_file = ''
 
 
 def start():
-    global i
+    global state
+    global select_file
     s = socket.socket()
     s.bind(('', 14801))
     s.listen(10)
@@ -14,19 +16,45 @@ def start():
         sc, address = s.accept()
         print('input connection: {0}'.format(address))
 
-        with open(path + 'file_%d.vf' % i, 'w+b') as file:
-            while True:
-                l = sc.recv(1024)
-                if not l:
-                    i += 1
-                    break
+        while True:
+            try:
+                command = sc.recv(1024).decode('utf-8')
+            except:
+                sc.close()
+                break
 
-                while l:
-                    file.write(l)
-                    l = sc.recv(1024)
+            if command == 'create':
+                state = 'CREATE_FILE'
+            elif command == 'loadfile':
+                state = 'LOAD_FILE'
+            elif command == 'writeinselectfile':
+                state = 'WRITE_IN_SELECT_FILE'
 
-        sc.close()
-
+            if state == 'CREATE_FILE':
+                file = sc.recv(1024).decode('utf-8')
+                select_file = path + file
+                open(select_file, 'w+b').close()
+            elif state == 'LOAD_FILE':
+                file = sc.recv(1024).decode('utf-8')
+                with open(path + file, 'r+b') as file:
+                    while True:
+                        data = file.read(1024)
+                        while data:
+                            sc.send(data)
+                            data = file.read(1024)
+                        if not data:
+                            break
+                sc.close()
+            elif state == 'WRITE_IN_SELECT_FILE':
+                with open(select_file, 'w+b') as file:
+                    while True:
+                        l = sc.recv(1024)
+                        while l:
+                            file.write(l)
+                            l = sc.recv(1024)
+                        if not l:
+                            break
+                sc.close()
     s.close()
 
 start()
